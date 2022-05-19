cf3f9917de63284562559ece20c87c67d6ee84f1
Tu Nguyen Anh <tu.lqd.163@gmail.com>
2022-05-19 11:57:53 +0700

    add data for subcriptions

--- TrickyBookStore/TrickyBookStore.Services/Store/Subscriptions.cs
+++ TrickyBookStore/TrickyBookStore.Services/Store/Subscriptions.cs
@@ -7,45 +7,62 @@ namespace TrickyBookStore.Services.Store
     {
         public static readonly IEnumerable<Subscription> Data = new List<Subscription>
         {
-            new Subscription { Id = 1, SubscriptionType = SubscriptionTypes.Paid, Priority = ...,
+             new Subscription { Id = 2, SubscriptionType = SubscriptionTypes.Free, Priority = 1,
                 PriceDetails = new Dictionary<string, double>
                 {
-                   
+                    {"FixPrice", 0 },
+                    {"OldBookDiscountPercent", 10 },
+                    {"NewBookDiscountPercent", 0 },
+                    {"DiscountedBooks", 0 }
                 }
             },
-            new Subscription { Id = 2, SubscriptionType = SubscriptionTypes.Free, Priority = ...,
+            new Subscription { Id = 1, SubscriptionType = SubscriptionTypes.Paid, Priority = 2,
                 PriceDetails = new Dictionary<string, double>
                 {
-
+                    {"FixPrice", 50 },
+                    {"OldBookDiscountPercent", 95 },
+                    {"NewBookDiscountPercent", 5 },
+                    {"DiscountedBooks", 3 }
                 }
             },
-            new Subscription { Id = 3, SubscriptionType = SubscriptionTypes.Premium, Priority = ...,
+            new Subscription { Id = 4, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = 3,
                 PriceDetails = new Dictionary<string, double>
                 {
-                   
-                }
-            },
-            new Subscription { Id = 4, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = ...,
-                PriceDetails = new Dictionary<string, double>
-                {
-                  
-                }              
-            },
-            new Subscription { Id = 5, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = ...,
-                PriceDetails = new Dictionary<string, double>
-                {
-                    { "FixPrice", 75 },
-                    ....
+                    {"FixPrice", 75 },
+                    {"OldBookDiscountPercent", 100 },
+                    {"NewBookDiscountPercent", 15 },
+                    {"DiscountedBooks", 3 }
                 },
                 BookCategoryId = 1
             },
-            new Subscription { Id = 6, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = ...,
+            new Subscription { Id = 5, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = 3,
                 PriceDetails = new Dictionary<string, double>
                 {
-                    { "FixPrice", 75 },
-                    ....
+                    {"FixPrice", 75 },
+                    {"OldBookDiscountPercent", 100 },
+                    {"NewBookDiscountPercent", 15 },
+                    {"DiscountedBooks", 3 }         
+                },
+                BookCategoryId = 2
+            },
+            new Subscription { Id = 6, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = 3,
+                PriceDetails = new Dictionary<string, double>
+                {
+                    {"FixPrice", 75 },
+                    {"OldBookDiscountPercent", 100 },
+                    {"NewBookDiscountPercent", 15 },
+                    {"DiscountedBooks", 3 }
                 },
                 BookCategoryId = 3
+            },
+            new Subscription { Id = 3, SubscriptionType = SubscriptionTypes.Premium, Priority = 4,
+                PriceDetails = new Dictionary<string, double>
+                {
+                    {"FixPrice", 200 },
+                    {"OldBookDiscountPercent", 100 },
+                    {"NewBookDiscountPercent", 15 },
+                    {"DiscountedBooks", 3 }
+                }
             }
         };
     }
