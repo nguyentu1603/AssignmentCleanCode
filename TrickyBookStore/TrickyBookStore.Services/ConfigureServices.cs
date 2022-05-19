09bc4890f72572b09c3d1ee9e33880af190a8106
Tu Nguyen Anh <tu.lqd.163@gmail.com>
2022-05-19 18:04:01 +0700

    add DI Configure Services

---
+++ TrickyBookStore/TrickyBookStore.Services/ConfigureServices.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TrickyBookStore.Services.Books;
+using TrickyBookStore.Services.Customers;
+using TrickyBookStore.Services.Payment;
+using TrickyBookStore.Services.PurchaseTransactions;
+using TrickyBookStore.Services.Subscriptions;
+
+namespace TrickyBookStore.Services
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IBookService), typeof(BookService));
+            services.AddScoped(typeof(ICustomerService), typeof(CustomerService));
+            services.AddScoped(typeof(IPaymentService), typeof(PaymentService));
+            services.AddScoped(typeof(IPurchaseTransactionService), typeof(PurchaseTransactionService));
+            services.AddScoped(typeof(ISubscriptionService), typeof(SubscriptionService));
+            return services;
+        }
+    }
+}
